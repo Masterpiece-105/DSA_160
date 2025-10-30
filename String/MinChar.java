dc37fe1f07e816552d289de6726d6773d9ab6b2b
shubham Yadav <sy907219@gmail.com>
2025-10-30 22:15:04 +0530

    MinCharacter

---
+++ String/MinChar.java
@@ -0,0 +1,31 @@
+package GFG160.String;
+
+
+public class MinChar {
+
+    static boolean isPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+    static int minChar(String s){
+        int cnt = 0;
+        int i = s.length() -1;
+        while(i >= 0 && !isPalindrome(s,0, i)){
+            i--;
+            cnt++;
+
+        }
+        return cnt++;
+    }
+    public static void main(String[] arg){
+        String s= "abc";
+        System.out.println(minChar(s));
+    }
+}
