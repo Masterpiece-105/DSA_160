2bcb47cab0126cedee0e3cfc004533ea3898ece4
shubham Yadav <sy907219@gmail.com>
2025-11-02 10:57:00 +0530

    Hindex Program

---
+++ Sorting/HIndex.java
@@ -0,0 +1,22 @@
+package GFG160.Sorting;
+
+import java.util.Arrays;
+
+public class HIndex {
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int idx = 0;
+
+        // keeping idx till citations [n-1-idx] > idx
+        while (idx < n && citations[n - 1 - idx] > idx) {
+            idx++;
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        int[] citations = {6, 0, 3, 5, 3};
+        System.out.println(hIndex(citations));
+    }
+}
