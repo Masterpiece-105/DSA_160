4325dc93bf1baa41318c6f18bceb63322d01f530
shubham Yadav <sy907219@gmail.com>
2025-11-22 01:19:34 +0530

    FindMin

---
+++ Searching/FindMin.java
@@ -0,0 +1,22 @@
+package GFG160.Searching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FindMin {
+    public static int findMin(int[] arr) {
+        int res = arr[0];
+        for (int i = 1; i < arr.length - 1; i++) {
+            res = Math.min(res, arr[i]);
+        }
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 1, 2, 3, 4};
+        System.out.println(findMin(arr));
+    }
+}
